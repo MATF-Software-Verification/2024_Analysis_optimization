0ab5b8e1f2b244b30325e240f8252e7ce5d58331
Pavle Vlajkovic <pavle@localhost.localdomain>
2025-08-10 08:03:40 +0200

    bugfix: Cppcheck script interpeter malfunction

--- Cppcheck/run_cppcheck.sh
+++ Cppcheck/run_cppcheck.sh
@@ -1,11 +1,12 @@
-#!bin/bash 
+#!/bin/bash
 
 OUTPUT_FILE_NAME="cppcheck_output.xml"
 
 echo "Traversing to correct directory..."
-cd ./include/simpleGraphs 
+cd ./include/simpleGraphs
 
-echo "Running Cppcheck..." 
-cppcheck --enable=all --xml --xml-version=2 simpleGraphs/simpleGraphs.hpp 2> $OUTPUT_FILE_NAME
+echo "Running Cppcheck..."
+cppcheck --enable=all --xml --xml-version=2 simpleGraphs/simpleGraphs.hpp 2>$OUTPUT_FILE_NAME
 
 echo 'Check report in ${OUTPUT_FILE_NAME}'
+
